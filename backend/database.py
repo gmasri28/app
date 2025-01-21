58479c25fe330c7dfbc75a34bdbcf084a7f0e1e6
Gabriel Masri <gmasri28@gmail.com>
2025-01-20 23:31:40 -0600

    Migrando base de datos a PostgreSQL

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./todo_app.db"
+DATABASE_URL = "postgresql://todo_db_uemr_user:xrQUNwaYFHnPJhlJUfPHqOS5gNJbiI2k@dpg-cu7j19lds78s73asgjvg-a.oregon-postgres.render.com/todo_db_uemr"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
